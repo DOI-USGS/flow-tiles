e278c18d0ed0b933bc3b92b7f778f068706e4ddc
collnell <cnell@usgs.gov>
2022-05-24 17:40:25 -0500

    cut data for breaks

--- src/prep_data.R
+++ src/prep_data.R
@@ -1,18 +1,10 @@
-add_flow_condition <- function(data_in, date_start, date_end){
+add_flow_condition <- function(data_in, date_start, date_end, breaks){
   data_in %>% 
-    mutate(date = as.Date(dateTime),
-           cond = 
-             case_when(
-               per < 0.05 ~ 'Driest',
-               per >= 0.05 & per < 0.1 ~ "Drier",
-               per >= 0.1 & per < 0.25 ~ "Dry", 
-               per >= 0.25 & per < 0.75 ~ "Normal",
-               per >= 0.75 & per < 0.9 ~ "Wet", 
-               per >= 0.9 & per < 0.95 ~ "Wetter", 
-               per >= 0.95 ~ "Wettest"
-             ),
-           cond = factor(cond, ordered = TRUE, levels = c("Driest", "Drier", "Dry", "Normal","Wet","Wetter", "Wettest"))) %>%
-    filter(date >= date_start, date <= date_end, !is.na(cond)) 
+    mutate(date = as.Date(dateTime)) %>%
+    filter(date >= date_start, date <= date_end, !is.na(per)) %>%
+    mutate(percentile_bin = cut(per, breaks = breaks, include.lowest = TRUE),
+           percentile_cond = factor(percentile_bin, labels = c("Driest", "Drier", "Dry", "Normal","Wet","Wetter", "Wettest")))
+  
 }
 
 site_count_state <- function(data_in, dv_site){
@@ -33,7 +25,7 @@ site_count_national <- function(data_in){
 
 flow_by_day <- function(data_in, sites_national) {
   data_in %>%
-    group_by(date, cond) %>%
+    group_by(date, percentile_cond, percentile_bin) %>%
     summarize(n_gage = length(unique(site_no))) %>%
     left_join(sites_national) %>%
     mutate(prop = n_gage/total_gage)
@@ -42,13 +34,13 @@ flow_by_day <- function(data_in, sites_national) {
 flow_by_day_by_state <- function(data_in, dv_site, sites_state) {
   data_in  %>%
     left_join(dv_site) %>% # adds state info for each gage
-    group_by(state_cd, date, cond) %>% # aggregate by state, day, flow condition
+    group_by(state_cd, date, percentile_cond) %>% # aggregate by state, day, flow condition
     summarize(n_gage = length(unique(site_no)))  %>%
     left_join(sites_state) %>% # add total_gage
     mutate(prop = n_gage/total_gage) %>% # proportion of gages
-    pivot_wider(id_cols = !n_gage, names_from = cond, values_from = prop, values_fill = 0) %>% # complete data for timepoints with 0 gages
+    pivot_wider(id_cols = !n_gage, names_from = percentile_cond, values_from = prop, values_fill = 0) %>% # complete data for timepoints with 0 gages
     pivot_longer(cols = c("Normal", "Wet", "Wetter", "Wettest", "Driest", "Drier", "Dry"), 
-                 names_to = "cond", values_to = "prop")
+                 names_to = "percentile_cond", values_to = "prop")
 
 }
 
